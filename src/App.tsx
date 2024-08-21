04baacce2d0bd8bd19d82d3d7b49d75f66ee74e3
Letícia <virgilioleticia240902@gmail.com>
2024-08-21 13:03:17 -0300

    Componente Home-CSS

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Home from "./pages/home/home";
+
+function App() {
+  // Código TypeScript
+
+  return (
+   // Código TSX => HTML + CSS
+    <>
+       
+      <Home/>
+      
+
+    </>
+
+  )
+}
+
+export default App
