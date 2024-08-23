f5cfc1ee573db29cf1845bd4e5c3271e387f757b
Letícia <virgilioleticia240902@gmail.com>
2024-08-23 15:22:43 -0300

    Componentes Navbar e Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,16 @@
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
 
 function Footer() {
+
+    let data = new Date().getFullYear()
  
+    
     return (
       <>
           <div className="flex justify-center bg-emerald-600 text-white">
             <div className="container flex flex-col items-center py-4">
               <p className=" text-xl font-bold"> 
-                Blog pessoal Generation | Copyright: </p>
+                Blog pessoal Generation | Copyright: {data} </p>
               <p className="text-lg">Acesse nossas redes sociais</p>
               <div className="flex gap-2">
                 <LinkedinLogo size={48} weight="bold" />
